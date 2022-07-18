c9ab86ff1363687cf29435c756f54200e4164208
HOSTNAME47 <mouadstev1@gmail.com>
2022-07-18 17:58:23 +0100

    build a simple image wallpaper component UI

---
+++ index.js
@@ -0,0 +1,13 @@
+let page = 1;
+let perpage = 20;
+
+// fetch(`https://api.pexels.com/v1/search?query=forest&page=${page}&per_page=${perpage}`, {
+//     method: 'get',
+//     Authorization: '563492ad6f917000010000015f23a59eb1b1425e88387a9ba1fcb3de',
+// })
+//     .then((response => {
+//         return response.json();
+//     }))
+//     .then(data => {
+//         console.log(data);
+//     });
